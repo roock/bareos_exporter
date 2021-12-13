bbb375fa4458d79fb692a7703911e881b80f6024
Tobias Wolter <tobias.wolter@b1-systems.de>
2021-12-13 14:13:05 +0100

    Switch to pgx and define type explicitly
    With this, we're doing a couple of things:
    
    1. Use `jackc/pgx` for PostgreSQL connection.
    2. Explicitly define the database type on the command line to facilitate using
    correct DSN strings.
    3. Switch to PostgreSQL as the default database type (MariaDB is deprecated as
    of Bareos 19 anyway)

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
@@ -12,34 +11,21 @@ import (
 
 	_ "github.com/go-sql-driver/mysql"
 	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
 )
-
-var connectionString string
 
 var (
 	exporterPort     = flag.Int("port", 9625, "Bareos exporter port")
 	exporterEndpoint = flag.String("endpoint", "/metrics", "Bareos exporter endpoint")
 	databaseURL      = flag.String("dsn", "mysql://bareos@unix()/bareos?parseTime=true", "Bareos database DSN")
+	databaseType     = flag.String("dbtype", "pgx", "mysql for mysql, pgx for postgres")
 	jobDiscoveryDays = flag.Int("job-discovery-days", 7, "Number of days in the past that will be searched for jobs")
 )
-
-func splitDsn(dsn string) (string, string, error) {
-	var splitDsn = strings.SplitN(dsn, "://", 2)
-	if len(splitDsn) != 2 {
-		return "", "", fmt.Errorf("Database DSN is incomplete: missing protocol: %s", dsn)
-	}
-	return splitDsn[0], splitDsn[1], nil
-}
 
 func main() {
 	flag.Parse()
 
-	dbType, connectionString, err := splitDsn(*databaseURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	connection, err := GetConnection(dbType, connectionString, *jobDiscoveryDays)
+	connection, err := GetConnection(*databaseType, *databaseURL, *jobDiscoveryDays)
 	if err != nil {
 		log.Fatal(err)
 	}
