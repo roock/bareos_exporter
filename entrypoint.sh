7a2ff33aa10bb64ff5f77fc04e0790cb94f9c21a
Marc BENSLAHDINE <mbenslahdine@veepee.com>
2022-04-03 01:57:27 +0200

    fix db_type

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ sleep ${WAIT_FOR_DB}
 
 # Run Dockerfile CMD
 if [ -z "$@" ]; then
-  if [ "${DB_TYPE}" == 'posgtres' ]; then
+  if [ "${DB_TYPE}" == 'postgres' ]; then
     exec ./bareos_exporter -endpoint ${ENDPOINT} -job-discovery-days ${JOB_DAYS} -port ${PORT} -dsn "${DB_TYPE}://host=${DB_HOST} dbname=${DB_NAME} sslmode=${SSL_MODE} user=${DB_USER} password=${DB_PASSWORD} port=${DB_PORT} ${EXTRA_OPTS}"
   else
     exec ./bareos_exporter -endpoint ${ENDPOINT} -job-discovery-days ${JOB_DAYS} -port ${PORT} -dsn "${DB_TYPE}://${DB_USER}:${DB_PASSWORD}@tcp(${DB_HOST}:${DB_PORT})/${DB_NAME}"
